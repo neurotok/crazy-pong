6f971c270cb45eade0a385f1fb0bc15d2f5114ad
neurotok <marcin@pingpongcat.com>
2019-04-23 10:37:36 +0200

    debug

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	gcc main.c -std=c99 -pedantic -g -Wall -o phong -lm -lSDL2
+	gcc main.c -O2 -std=c99 -pedantic -g -Wall -o phong -lm -lSDL2 -lSDL2_gfx
 clear:
 	rm ./phong
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdbool.h>
 #include <math.h>
 #include <time.h>
@@ -48,7 +49,8 @@ int main(void)
 	bool did_touch = false;
 
 	srand(time(NULL));
-	float vector[2] = {-1.0, (-4 + rand() % 9) * 0.1};
+	float vector[2] = {-1.0f, 0.0f};
+	//float vector[2] = {-1.0, (-4 + rand() % 9) * 0.1};
 
 
 	SDL_Renderer *left = SDL_CreateRenderer(left_window,-1,SDL_RENDERER_ACCELERATED);
@@ -75,7 +77,6 @@ int main(void)
 		}
 
 	}
-
 
 	SDL_Event event;
 
@@ -116,115 +117,8 @@ int main(void)
 		SDL_RenderClear(left);
 		SDL_RenderClear(right);
 
-		SDL_SetRenderDrawColor(left,255,255,255,255);
-		SDL_SetRenderDrawColor(right,255,255,255,255);
-
-		SDL_RenderFillRect(left, &player_1);
-
-		//player_2.y = right_ball.y + PAD_HEIGH / 2;
-		SDL_RenderFillRect(right, &player_2);
-		SDL_RenderFillRect(left, &player_1);
-
-
-		//Ball movment
-		if (is_left) {
-			left_ball_x += vector[0];
-			left_ball_y += vector[1];
-		}else{
-			right_ball_x += vector[0];
-			right_ball_y += vector[1];
-		}
-
-		left_ball.x = (int)left_ball_x;
-		left_ball.y = (int)left_ball_y;
-		SDL_RenderFillRect(left, &left_ball);
-
-		right_ball.x = (int)right_ball_x;
-		right_ball.y = (int)right_ball_y;
-		SDL_RenderFillRect(right, &right_ball);
-
-		//Freeze left ball
-		if(is_left == true && left_ball.x > WINDOW_WIDTH + BALL_SIZE){
-			left_ball.x = WINDOW_WIDTH + BALL_SIZE;	
-			is_left = !is_left;
-			right_ball.x = -BALL_SIZE;
-			right_ball.y = left_ball.y;
-			did_touch = false;
-		}
-		//Freeze right ball
-		if(is_left == false && right_ball.x < -BALL_SIZE){
-			right_ball.x = -BALL_SIZE;;	
-			is_left = !is_left;
-			left_ball.x = WINDOW_WIDTH + BALL_SIZE;
-			left_ball.y = right_ball.y;
-			did_touch = false;
-		}
-
-		//Player bounce
-		if (is_left == true && did_touch == false && left_ball.y + BALL_SIZE > player_1.y && left_ball.y  < player_1.y + PAD_HEIGH && left_ball.x < 20 + PAD_WIGHT) {
-			int d = (left_ball.y + BALL_SIZE / 2) - (player_1.y + PAD_HEIGH / 2);
-			printf("Left bounce at %d\n",d);
-			vector[0] = -vector[0];
-			vector[1] = vector[1];
-			did_touch = true;
-
-		}
-		if(is_left == false && did_touch == false && right_ball.y + BALL_SIZE > player_2.y && right_ball.y < player_2.y + PAD_HEIGH && right_ball.x == WINDOW_WIDTH - 40){
-
-			int d = (right_ball.y + BALL_SIZE / 2) - (player_2.y + PAD_HEIGH / 2);
-
-			printf("Right bounce at %d\n", d);
-			vector[0] = -vector[0];
-			vector[1] = -vector[1];
-			did_touch = true;
-		}	
-		//Bottom || top edge bounce
-		if (right_ball.y < 0 || right_ball.y + BALL_SIZE > WINDOW_HEIGHT || left_ball.y < 0 || left_ball.y + BALL_SIZE > WINDOW_HEIGHT) {
-			vector[0] = vector[0];
-			vector[1] = vector[1];
-
-		}
-
-
-		//Score
-		if (is_left == true && left_ball.x + 10 < 0) {
-			if (score[1] < 9) {
-				score[1]++;
-
-			}	
-			else{
-				score[0] = 0;
-				score[1] = 0;
-
-			}
-
-			left_ball_x = WINDOW_WIDTH - BALL_SIZE;
-			left_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
-			right_ball_x = -10;
-			right_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
-			vector[0] = -1.0f;
-			vector[1] = (-4 + rand() % 9) * 0.1;
-
-		}
-		if (is_left == false && right_ball.x  > WINDOW_WIDTH) {
-			if (score[0] < 9) {
-				score[0]++;
-
-			}	
-			else{
-				score[0] = 0;
-				score[1] = 0;
-
-			}
-			left_ball_x = WINDOW_WIDTH + BALL_SIZE;
-			left_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
-			right_ball.x = -10;
-			right_ball.y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
-			vector[0] = -1.0f;
-			vector[1] = (-4 + rand() % 9) * 0.1;
-
-		}
-
+		SDL_SetRenderDrawColor(left,92,92,92,255);
+		SDL_SetRenderDrawColor(right,92,92,92,255);
 
 		//Score rendering
 		for (int i = 0; i < 15; ++i) {
@@ -237,10 +131,146 @@ int main(void)
 			}
 		}
 
-		//SDL_SetWindowPosition(right_window,wp,100); 
+
+		SDL_SetRenderDrawColor(left,255,255,255,255);
+		SDL_SetRenderDrawColor(right,255,255,255,255);
+
+		SDL_RenderFillRect(left, &player_1);
+		//player_2.y = right_ball.y + PAD_HEIGH / 2;
+		SDL_RenderFillRect(right, &player_2);
+
+
+		//Ball movment
+		if (is_left) {
+			left_ball_x += vector[0];
+			left_ball_y += vector[1];
+		}else{
+			right_ball_x += vector[0];
+			right_ball_y += vector[1];
+		}
+
+		///Ball target initialization & render
+		left_ball.x = (int)left_ball_x;
+		left_ball.y = (int)left_ball_y;
+		SDL_RenderFillRect(left, &left_ball);
+
+		right_ball.x = (int)right_ball_x;
+		right_ball.y = (int)right_ball_y;
+		SDL_RenderFillRect(right, &right_ball);
+
+		//Freeze left ball
+		if(is_left == true && left_ball_x > WINDOW_WIDTH + BALL_SIZE){
+			left_ball_x = WINDOW_WIDTH + BALL_SIZE;	
+			is_left = !is_left;
+			right_ball_x = -BALL_SIZE;
+			right_ball_y = left_ball_y;
+			did_touch = false;
+		}
+		//Freeze right ball
+		if(is_left == false && right_ball_x < -BALL_SIZE){
+			right_ball_x = -BALL_SIZE;;	
+			is_left = !is_left;
+			left_ball_x = WINDOW_WIDTH + BALL_SIZE;
+			left_ball_y = right_ball_y;
+			did_touch = false;
+		}
+
+		//Player bounce
+		if (is_left == true && did_touch == false && left_ball.y + BALL_SIZE > player_1.y && left_ball.y  < player_1.y + PAD_HEIGH && left_ball.x < 20 + PAD_WIGHT) {
+			int d = (left_ball_y + BALL_SIZE / 2) - (player_1.y + PAD_HEIGH / 2);
+			//printf("Left bounce at %d\n",d);
+			vector[0] = -vector[0];
+			vector[1] = vector[1];
+			vector[1] += d * 0.5 / (PAD_HEIGH / 2);
+			did_touch = true;
+
+		}
+		if(is_left == false && did_touch == false && right_ball.y + BALL_SIZE > player_2.y && right_ball.y < player_2.y + PAD_HEIGH && right_ball_x == WINDOW_WIDTH - 40){
+
+			int d = (right_ball.y + BALL_SIZE / 2) - (player_2.y + PAD_HEIGH / 2);
+			vector[0] = -vector[0];
+			vector[1] = -vector[1];
+			vector[1] += d * 0.5 / (PAD_HEIGH / 2);
+			did_touch = true;
+		}	
+
+		//Bottom || top edge bounce
+		if (right_ball_y < 0 || right_ball_y + BALL_SIZE > WINDOW_HEIGHT || left_ball_y < 0 || left_ball_y + BALL_SIZE > WINDOW_HEIGHT) {
+
+			vector[1] = -vector[1];
+			printf("bounce\n");
+
+		}
+
+
+		//Score
+		if (is_left == true && did_touch  == false && left_ball_x + 10 < 0) {
+
+			if (score[1] < 9) {
+				score[1]++;
+
+			}	
+			else{
+				score[0] = 0;
+				score[1] = 0;
+			}
+			left_ball_x = WINDOW_WIDTH - BALL_SIZE;
+			left_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
+			right_ball_x = -10;
+			right_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
+			vector[0] = -1.0f;
+			vector[1] = 0.0f;
+			//vector[1] = (-4 + rand() % 9) * 0.1;
+			is_left = true;
+			//did_touch = false;
+			
+			printf("score\n");
+
+		}
+		if (is_left == false  && did_touch == false && right_ball_x  > WINDOW_WIDTH) {
+
+			if (score[0] < 9) {
+				score[0]++;
+
+			}	
+			else{
+
+				score[0] = 0;
+				score[1] = 0;
+
+			}
+			left_ball_x = WINDOW_WIDTH + BALL_SIZE;
+			left_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
+			right_ball_x = -10;
+			right_ball_y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
+			vector[0] = -1.0f;
+			vector[1] = 0.0f;
+			//vector[1] = (-4 + rand() % 9) * 0.1;
+			is_left = true;
+			//did_touch = false;
+
+
+			printf("score\n");
+		}
+		
+		int y = 20, x = 20;
+
+		boxColor(left, x, y, x + 200, y + 100, 0xAA000000);	
+
+		char d_current_state[128];
+		sprintf(d_current_state,"Vector X: %.2f",vector[0]);	
+		stringColor (left, x + 10, y + 10, d_current_state,0xFFFFFFFF); 
+
+		char d_win_counter[128];
+		sprintf(d_win_counter,"Vector Y: %.2f", vector[1]);	
+		stringColor (left, x + 10, y + 25, d_win_counter,0xFFFFFFFF); 
+
+
+	
 		SDL_RenderPresent(left);
 		SDL_RenderPresent(right);
 
+		//SDL_SetWindowPosition(right_window,wp,100); 
 		//SDL_Delay(10);
 
 
